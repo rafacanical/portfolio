dc74e2245bb06b7c70ada0b247164b2a7d633644
Rafael Morais <rafaelcanical@gmail.com>
2022-06-20 00:10:59 +0200

    Code Highlight theme & converted more components to React

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,18 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), react(), tailwind()]
+  integrations: [preact(), react(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'min-light',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  }
 })
